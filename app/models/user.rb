a4078d815944b3c88078bdca5b565ffb5799b5f0
igorianich <luzniak9@gmail.com>
2024-06-05 17:17:33 +0300

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   rolify
+
+  validates :first_name, :last_name, :phone, :email, presence: true
+  validates :email, uniqueness: true
+  validates :phone, length: { is: 10 }, numericality: { only_integer: true }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
